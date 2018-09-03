68ba3613506ab37b1c50f49f73486935d749dd18
ryan <ryangiomi@gmail.com>
2018-09-03 14:56:00 -0700

    Add prefabs, scripts, and misc updates

---
+++ proto-battle-1/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate () {
+        float move = Input.GetAxis("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+    }
+}
